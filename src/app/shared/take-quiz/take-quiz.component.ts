c81d6d26806e5bf9407624cd3474223fb28eb705
StoilTomanov <stoiltomanov@gmail.com>
2022-04-15 21:04:55 +0300

    created quiz

--- src/app/shared/take-quiz/take-quiz.component.ts
+++ src/app/shared/take-quiz/take-quiz.component.ts
@@ -1,5 +1,8 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ILesson } from 'src/app/interfaces';
+import { LessonsService } from '../lessons.service';
 
 
 @Component({
@@ -8,12 +11,29 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./take-quiz.component.css']
 })
 export class TakeQuizComponent implements OnInit {
+  lessonData!: any[];
+  hasCompleted: string = ''
+  @ViewChild('takeQuizForm') form!: NgForm;
+
 
   constructor(
+    private lessonService: LessonsService,
     @Inject(MAT_DIALOG_DATA) public data: any,
   ) { }
 
   ngOnInit(): void {
+
+    this.lessonService.getLessonById$(this.data)
+      .subscribe(data => this.lessonData = data.quiz);
+  }
+
+  onTakeQuizSubmit(event: Event): void {
+    const quizData = this.form.value;
+    const lessonId = (event.target as Element).id;
+
+    this.lessonService.compareQuizResults$(lessonId, quizData)
+      .subscribe(data => this.hasCompleted = data);
+      
   }
 
 }
