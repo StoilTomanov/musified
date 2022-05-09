65d2293d1645b04da4ca2c2797a450ed97ffb6d4
StoilTomanov <stoiltomanov@gmail.com>
2022-05-09 16:47:55 +0300

    video

--- src/app/shared/course-details/video-preview/video-preview.component.ts
+++ src/app/shared/course-details/video-preview/video-preview.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ILesson } from 'src/app/interfaces';
+import { LessonsService } from '../../lessons.service';
 
 @Component({
   selector: 'app-video-preview',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-preview.component.css']
 })
 export class VideoPreviewComponent implements OnInit {
+  lessonById: ILesson | undefined;
 
-  constructor() { }
+  constructor(
+    private lessonService: LessonsService,
+    private activatedRouter: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.lessonService.getLessonById$(this.activatedRouter.snapshot.params['id'])
+      .subscribe(data => this.lessonById = data);
   }
 
 }
